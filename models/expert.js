8e747d0d07e74be9a76d270f727bacbf410452f6
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-06-03 12:07:10 +0200

    Add models and migrations

---
+++ models/expert.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Expert extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Expert.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    job: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Expert',
+  });
+  return Expert;
+};
---
+++ models/presentation.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Presentation extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Presentation.init({
+    title: DataTypes.STRING,
+    starttime: DataTypes.DATE,
+    length: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Presentation',
+  });
+  return Presentation;
+};
