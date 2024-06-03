e6488865ae5b0e181439f4f734e267bbc58ca943
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-06-03 14:05:40 +0200

    6. feladat

--- routes/root.js
+++ routes/root.js
@@ -85,13 +85,41 @@ module.exports = function (fastify, opts, next) {
             if (await Expert.findOne({ where: { email } })) {
                 return reply.status(StatusCodes.CONFLICT).send();
             }
-            
+
             const expert = await Expert.create({
                 name, email, job
             });
 
             reply.status(StatusCodes.CREATED).send(expert);
         });
+
+
+    // 6. feladat
+    fastify.post("/login",
+        {
+            schema: {
+                body: {
+                    type: 'object',
+                    required: ['email'],
+                    properties: {
+                        email: { type: 'string' },
+                    }
+                }
+            }
+        },
+        async (request, reply) => {
+            const { email } = request.body;
+
+            const expert = await Expert.findOne({ where: { email } })
+
+            if (!expert) {
+                return reply.status(StatusCodes.NOT_FOUND).send();
+            }
+
+            reply.send({ token: fastify.jwt.sign(expert.toJSON()) });
+        });
+
+
 
 
 
